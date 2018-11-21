611b4a89063fb2aaa778965d9291c518ec6b182a
Matheus Couto <couto.matheus@outlook.com>
2018-11-20 23:40:58 -0200

    redux: put video on display from action

--- src/components/VideoList/index.tsx
+++ src/components/VideoList/index.tsx
@@ -1,39 +1,43 @@
 import * as React from 'react';
 import { IVideoItem } from 'declarations';
-import { database } from '../../services/firebase';
-import { assign } from 'lodash';
-import VideoLoader from './VideoLoader';
 import styles from './styles';
 import VideoItem from './VideoItem';
-import { IoIosArrowForward, IoIosArrowBack } from 'react-icons/io';
+import { assign } from 'lodash';
+import { database } from '../../services/firebase';
 import Modal from 'react-responsive-modal';
+import { removeVideoFromDisplay, putVideoOnDisplay, selectSelectVideoOnDisplay } from 'src/store/app/state';
+import { Dispatch } from 'redux';
+import { IRootState } from 'src/store';
+import { connect } from 'react-redux';
+import { IoIosArrowForward, IoIosArrowBack } from 'react-icons/io';
+import VideoLoader from './VideoLoader';
 
 interface IState {
   isLoadingVideos: boolean;
   videoList: IVideoItem[];
-  pagination: {
-    current: number,
-    total: number,
-  };
   isArquivingVideo: boolean;
   arquivingVideoTitle?: string;
   arquivingVideoId?: string;
   newVideoTitle: string;
   isEditingVideoTitle: boolean;
   editingVideoTitleId?: string;
+  pagination: {
+    current: number,
+    total: number,
+  }
 }
 
-class VideoList extends React.PureComponent<{},IState> {
+class VideoList extends React.PureComponent<IMapStateToProps & IMapDispatchToProps, IState> {
   public state:IState = {
     isLoadingVideos: true,
     videoList: [],
-    pagination: {
-      current: 1,
-      total: 1,
-    },
     isArquivingVideo: false,
     isEditingVideoTitle: false,
     newVideoTitle: '',
+    pagination: {
+      current: 1,
+      total: 1,
+    }
   }
 
   private dbPath = database().ref('/videos').orderByChild('creationTimeOrder');
@@ -41,7 +45,7 @@ class VideoList extends React.PureComponent<{},IState> {
   public componentDidMount() {
     this.dbPath.on('value', () => {
       this.dbPath.limitToFirst(3).once('value', (videoListSnapshot) => {
-        let videoList: any = [];
+        const videoList: any = [];
         if(videoListSnapshot) {
           videoListSnapshot.forEach(videoItem => { videoList.push(assign(videoItem.val(), { id: videoItem.key }))})
         }
@@ -63,9 +67,11 @@ class VideoList extends React.PureComponent<{},IState> {
 
   public componentWillUnmount() {
     this.dbPath.off('value');
+    this.props.removeVideoFromDisplay();
   }
+
 	public render() {
-    const { editingVideoTitleId, isEditingVideoTitle, newVideoTitle, isLoadingVideos, videoList, pagination, isArquivingVideo, arquivingVideoTitle, arquivingVideoId } = this.state;
+    const { pagination, isLoadingVideos, isArquivingVideo, arquivingVideoTitle, arquivingVideoId, newVideoTitle, isEditingVideoTitle, editingVideoTitleId } = this.state;
 		return(
       <div style={styles.videoListWrapper}>
         <div style={styles.paginationButtonsWrapper}>
@@ -74,20 +80,21 @@ class VideoList extends React.PureComponent<{},IState> {
           <IoIosArrowForward size={23} onClick={pagination.current < pagination.total ? this.getPage('next') : undefined} style={styles.paginationButton} color={pagination.current >= pagination.total ? 'rgb(170, 170, 170)' : undefined} />
         </div>
         <div style={styles.videoList}>
-        {
-          isLoadingVideos
-          ? <VideoLoader />
-          : videoList.map((videoItem: IVideoItem) => (
-                        <VideoItem
-                          key={videoItem.id}
-                          title={videoItem.name}
-                          status={videoItem.status}
-                          arquiveVideo={this.handleOpenArquiveVideoModal(videoItem.id)}
-                          thumbnails={videoItem.thumbnails}
-                          editVideoTitle={this.handleOpenEditVideoTitleModal(videoItem.id)}
-                        />
-                      ))
-        }
+          {
+            isLoadingVideos
+            ? <VideoLoader />
+            : this.state.videoList.map((videoItem: IVideoItem) => <VideoItem
+                                                                    key={videoItem.id}
+                                                                    onSelectVideo={this.handlePutVideoOnDisplay(videoItem.id, videoItem.high)}
+                                                                    title={videoItem.name}
+                                                                    status={videoItem.status}
+                                                                    thumbnails={videoItem.thumbnails}
+                                                                    arquiveVideo={this.handleOpenArquiveVideoModal(videoItem.id)}
+                                                                    editVideoTitle={this.handleOpenEditVideoTitleModal(videoItem.id)}
+                                                                  />
+                                    )
+          }
+
         <Modal open={isArquivingVideo} styles={styles.modal} onClose={this.handleCloseArquiveVideoModal} center>
           <h2 style={styles.modalTitle}>Are you sure you want to delete "{arquivingVideoTitle}" ?</h2>
           <div style={styles.modalOptionsWrapper}>
@@ -107,26 +114,26 @@ class VideoList extends React.PureComponent<{},IState> {
       </div>
 		);
   }
-  
+
   private getPage = (direction: 'next' | 'previous') => () => {
     this.setState(state => ({
       ...state,
       isLoadingVideos: true,
     }))
-    const { videoList } = this.state;
-    let pivotVideo: IVideoItem, rootRef:database.Query; 
+    let pivotVideo: IVideoItem;
+    let rootRef:database.Query; 
 
     if(direction==='next') {
-      pivotVideo = videoList[videoList.length - 1];
+      pivotVideo = this.state.videoList[this.state.videoList.length - 1];
       rootRef = this.dbPath.limitToFirst(4).startAt(pivotVideo.creationTimeOrder)
     } else {
-      pivotVideo = videoList[0];
+      pivotVideo = this.state.videoList[0];
       rootRef = this.dbPath.limitToLast(4).endAt(pivotVideo.creationTimeOrder)
     }
     
     rootRef.once('value').then((videoListSnapshot) => {
 
-      let videoList: any = [];
+      const videoList: any = [];
       if(videoListSnapshot) {
         videoListSnapshot.forEach(videoItem => { if(videoItem.key !== pivotVideo.id) { videoList.push(assign(videoItem.val(), { id: videoItem.key }))}})
       }
@@ -144,6 +151,10 @@ class VideoList extends React.PureComponent<{},IState> {
         }))
       })
     })
+  }
+
+  private handlePutVideoOnDisplay = (videoId: string, videoUrl: string) => () => {
+    this.props.putVideoOnDisplay(videoUrl, videoId)
   }
 
   private handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -194,13 +205,16 @@ class VideoList extends React.PureComponent<{},IState> {
 
   private handleArquiveVideo = (videoId: string) => () => {
     database().ref(`/videos/${videoId}`).once('value').then((videoItemSnapshot) => {
-      let updates: any = {};
+      const updates: any = {};
       updates[`/videos/${videoId}`] = null;
       updates[`/arquivedVideos/${videoId}`] = videoItemSnapshot.val();
       database().ref('/videosCount').once('value').then((videosCountSnapshot) => {
         const videosCount = videosCountSnapshot.val();
         updates[`/videosCount`] = videosCount - 1;
         database().ref().update(updates).then(() => {
+          if(this.props.videoOnDisplay && this.props.videoOnDisplay.id === videoId) {
+            this.props.removeVideoFromDisplay()
+          }
           this.handleCloseArquiveVideoModal()
         })
       })
@@ -208,4 +222,30 @@ class VideoList extends React.PureComponent<{},IState> {
   }
 }
 
-export default VideoList;
+/* *************************** */
+//      MAP STATE TO PROPS     //
+/* *************************** */
+
+interface IMapStateToProps {
+	videoOnDisplay?: {url: string, id: string};
+};
+
+const mapStateToProps = (state: IRootState): IMapStateToProps => ({
+	videoOnDisplay: selectSelectVideoOnDisplay(state)
+});
+
+/* *************************** */
+//    MAP DISPATCH TO PROPS    //
+/* *************************** */
+
+interface IMapDispatchToProps {
+	putVideoOnDisplay: (url: string, id: string) => void;
+	removeVideoFromDisplay: () => void;
+};
+
+const mapDispatchToProps = (dispatch: Dispatch): IMapDispatchToProps => ({
+	putVideoOnDisplay: (url, id) => dispatch(putVideoOnDisplay({url, id})),
+	removeVideoFromDisplay: () => dispatch(removeVideoFromDisplay()),
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(VideoList);
