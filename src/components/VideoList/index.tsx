2a07ad7bb1bcc5446e858dd638a121c4e303c713
Matheus Couto <couto.matheus@outlook.com>
2018-11-20 23:12:28 -0200

    feat: fetching uploaded videos from firebase

--- src/components/VideoList/index.tsx
+++ src/components/VideoList/index.tsx
@@ -1,21 +1,62 @@
 import * as React from 'react';
+import { IVideoItem } from 'declarations';
+import { database } from '../../services/firebase';
+import { assign } from 'lodash';
+import VideoLoader from './VideoLoader';
 import styles from './styles';
 import VideoItem from './VideoItem';
 
-interface IProps {
-  onSelectVideo: (videoUrl: string) => () => void
+interface IState {
+  isLoadingVideos: boolean;
+  videoList: IVideoItem[];
 }
 
-class Videos extends React.PureComponent<IProps> {
+class VideoList extends React.PureComponent<{},IState> {
+  public state:IState = {
+    isLoadingVideos: true,
+    videoList: [],
+  }
+
+  private dbPath = database().ref('/videos').orderByChild('creationTimeOrder');
+
+  public componentDidMount() {
+    this.dbPath.on('value', () => {
+      this.dbPath.limitToFirst(3).once('value', (videoListSnapshot) => {
+        let videoList: any = [];
+        if(videoListSnapshot) {
+          videoListSnapshot.forEach(videoItem => { videoList.push(assign(videoItem.val(), { id: videoItem.key }))})
+        }
+        this.setState(state => ({
+          ...state,
+          videoList,
+          isLoadingVideos: false,
+        }))
+      })
+    })
+  }
+
+  public componentWillUnmount() {
+    this.dbPath.off('value');
+  }
 	public render() {
-    const { onSelectVideo } = this.props;
+    const { isLoadingVideos, videoList } = this.state;
 		return(
       <div style={styles.videoList}>
-        {/* TODO: Fetch video items from Firebase */}
-        <VideoItem onSelectVideo={onSelectVideo("")} title="Video title" status="done" thumbnails={[""]}/>
+      {
+        isLoadingVideos
+        ? <VideoLoader />
+        : videoList.map((videoItem: IVideoItem) => (
+                      <VideoItem
+                        key={videoItem.id}
+                        title={videoItem.name}
+                        status={videoItem.status}
+                        thumbnails={videoItem.thumbnails}
+                      />
+                    ))
+      }
       </div>
 		);
 	}
 }
 
-export default Videos;
+export default VideoList;
